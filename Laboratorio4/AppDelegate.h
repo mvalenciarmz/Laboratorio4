173213f25e64870b142b27166ec9902b3165c909
Marcos <marcos@Marcoss-Mac-mini.local>
2015-01-29 10:58:46 -0600

    Initial Commit

---
+++ Laboratorio4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Laboratorio4
+//
+//  Created by Marcos on 1/29/15.
+//  Copyright (c) 2015 Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
